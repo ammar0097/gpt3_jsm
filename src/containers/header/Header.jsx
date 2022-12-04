9af64c80d8143e33e6d9d09098fcf6438d95f26a
Ammar <ammarferchichi7@gmail.com>
2022-12-04 22:20:43 +0100

    navbar and the header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import './header.css';
-
+import React from "react";
+import "./header.css";
+import people from '../../assets/people.png';
+import ia from '../../assets/ai.png';
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <div className="gradient__text">
+          <h1> Let's Build Something amazing with GPT-3 OpenAi</h1>
+        </div>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your Email address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="gpt3__header-content__people"> 
+          <img src={people} alt="people"/> 
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+        <div className="gpt3__header-content__ai">
+          <img src={ia} alt="ia"/>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
