a8ce203330eef4ae78b470fc1cc63db2b90d2103
Ammar <ammarferchichi7@gmail.com>
2022-12-13 15:04:43 +0100

    whatgpt3

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import './whatGPT3.css';
-
+import React from "react";
+import "./whatGPT3.css";
+import Features from "../features/Features";
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
-  )
-}
+    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Features />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Features/>
+        <Features/>
+        <Features/>
+      </div>
+    </div>
+  );
+};
 
-export default WhatGPT3
+export default WhatGPT3;
